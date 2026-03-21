a64d087b1720db5e9ad51d30a89e568bd8d70c39
DdimaPos <postoroncadumitru@gmail.com>
2026-03-21 21:50:16 +0200

    features: configured search querying to duckduckgo

---
+++ go.mod
@@ -0,0 +1,5 @@
+module search
+
+go 1.25.3
+
+require golang.org/x/net v0.52.0 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import ()
+import (
+	"flag"
+	"search/search"
+)
 
 func main() {
+	var searchQuery string
+	var url string
+	flag.StringVar(&searchQuery, "s", "", "can introduce here the search query")
+	flag.StringVar(&url, "u", "", "specicify a url to make a request to that url")
+	flag.Parse()
 
+	search.MakeSearchRequest(string(searchQuery))
 }
